924285e40ba1c09ef124fcdfabfbd3fd56ff900b
Ankit-Mahata <ankitmahata44@gmail.com>
2025-09-17 13:41:34 +0545

    operator overloading

---
+++ Operator overloading/type_conversion_of_obj4.cpp
@@ -0,0 +1,22 @@
+// to convert user defined into fundamental data type
+#include<iostream>
+using namespace std;
+class celsius{
+    float cel;
+    public:
+    celsius(float c=0)
+    {cel=c;}
+    operator float()    // we can make normal function also;
+    
+    {
+        float x=1.8*cel+32;
+        return x;
+    }
+};
+int main()
+{
+    celsius c1(40);
+    float far;
+    far=c1;
+    cout<<"Temp in farhenit : "<<far;
+}
