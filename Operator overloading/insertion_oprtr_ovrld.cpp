d2b04191afab404a851945fd089114afa56e5210
Ankit-Mahata <ankitmahata44@gmail.com>
2025-09-18 13:30:05 +0545

    operator overloading

---
+++ Operator overloading/insertion_oprtr_ovrld.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class complex {
+    float real, imag;
+    public:
+    void input()
+    {
+        cout<<" Enter real and imaginary values : ";
+        cin>>real>>imag;
+    }
+    friend void operator <<(ostream &obj1, complex &obj2)
+    {
+        obj1<<" "<<obj2.real<<" + i"<<obj2.imag<<endl;
+    }
+};
+int main()
+{
+    complex c1,c2;
+    c1.input();
+    c2.input();
+    cout<<c1;
+    cout<<c2;
+}
